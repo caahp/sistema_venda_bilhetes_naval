ba6d6642acd23a73ed9677d3f1426063574574b6
pauloricf <pauloricf21@gmail.com>
2022-10-07 16:48:20 -0400

    interface

---
+++ controller/LoginController.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import View.Destinos;
+import View.Register;
+import View.Login;
+
+/**
+ *
+ * @author PauloR
+ */
+public class LoginController {
+
+    private final Login view;
+
+    public LoginController(Login view) {
+        this.view = view;
+    }
+    
+    public void goRegisterScreen(){
+        Register cadastro = new Register();
+        cadastro.setVisible(true);
+        this.view.dispose();
+    }
+    
+    public void goDestinosScreen(){
+        Destinos destinos = new Destinos();
+        destinos.setVisible(true);
+        this.view.dispose();
+    }
+    public void fiz(){
+        System.out.println("smsak");
+        this.view.exibeMensagem("Executei");
+       }
+    
+}
---
+++ controller/RegisterController.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import View.Login;
+import View.Register;
+
+/**
+ *
+ * @author PauloR
+ */
+public class RegisterController {
+    private final Register view;
+    
+    public RegisterController(Register view) {
+        this.view = view;
+    }
+    
+    public void goLoginScreen(){
+        Login login = new Login();
+        login.setVisible(true);
+        this.view.dispose();
+                
+    }
+    
+    
+        
+    
+}
